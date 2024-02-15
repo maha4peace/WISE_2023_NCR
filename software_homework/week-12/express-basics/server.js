ba5202d59db346d9672817af1a4a68f9e7796a22
Maha <maha.ahmed4peace@gmail.com>
2024-02-15 00:00:45 -0500

    adding gitignore file and express app set-up

--- software_homework/week-12/express-basics/server.js
+++ software_homework/week-12/express-basics/server.js
@@ -1,15 +1,30 @@
 const express = require('express');
+const responses =  require('./models/responses.js');
+const app =  express();
 
-//Create the express app 
-const app = express();
+app.get('/', function(req,res){
+    res.send(`<h1>Hello?!</h1>`);
+})
 
-// Define a "root" route directly on app
-app.get('/', function (req, res) {
-    res.send('<h1>Hello World!</h1>');
-  });
+app.get('/greeting', (req, res) => {
+    res.send('<h1>Hello, stranger!</h1>')
+})
 
-// Tell the app to listen on port 3000
-// for HTTP requests from clients
-app.listen(3000, function () {
-    console.log('Listening on port 3000');
-  });
+app.get('/greeting/:name', (req, res) => {
+    console.log(req. params);
+    res.send("Hello," + "" + req.params.name + "! It's so great to see you!");
+})
+
+app.get('/tip/:total/:percent', (req, res)=>{
+    console.log(req.params);
+    res.send("Based on the $"+req.params.total+" total and "+req.params.percent+"% tip, the tip will be $"+req.params.total*(req.params.percent/100))
+})
+
+app.get('/magic/:question', (req, res)=>{
+console.log(req.params);
+res.send(req.params.question + "?" + '<br/>'+ `<h1>${responses[Math.floor(Math.random()*responses.length)]}</h1>`);
+});
+
+app.listen(3000, function() {
+    console.log("Listening on the port 3000")
+})
