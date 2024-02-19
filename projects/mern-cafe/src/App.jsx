ae205be28aa0c71bdce3a6191e04177fb885364f
Maha <maha.ahmed4peace@gmail.com>
2024-02-19 00:12:24 -0500

    adding the skeleton level page components

---
+++ projects/mern-cafe/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [user, setUser] = useState(null) ; 
+
+  return (
+    <div>
+
+      
+    </div>
+  );
+}
+
+export default App;
