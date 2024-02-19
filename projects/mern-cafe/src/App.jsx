696ebc415c0e68aa7c381c9885e63d37c4bb04c9
Maha <maha.ahmed4peace@gmail.com>
2024-02-19 01:13:00 -0500

    starting the nav bar

--- projects/mern-cafe/src/App.jsx
+++ projects/mern-cafe/src/App.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import AuthPage from './pages/AuthPage';
 import OrderHistorypage from './pages/OrderHistoryPage';
 import NewOrderPage from './pages/NewOrderPage';
+import Nav from './components/Nav';
 import { Routes, Route} from 'react-router-dom'
 
 function App() {
